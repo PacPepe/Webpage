68a4c7cc77d7ad3e75ffb14ac36c4ebab59ae217
PacPepe <131310446+PacPepe@users.noreply.github.com>
2023-04-19 17:11:02 -0500

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const connectWalletButton = document.getElementById('connectWallet');
-connectWalletButton.addEventListener('click', async () => {
-  await loadWeb3();
-  await switchToArbitrum();
-  fetchHighScores();
-});
